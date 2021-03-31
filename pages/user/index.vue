9f0f6f13127b2045d14286e58d9fe01b4d157bc1
Maksim <blacklivi@yandex.by>
2021-03-31 19:51:29 +0300

    userProfile

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="blocks_test">
       <v-row class="blocks blocks_one">
-        <v-col
-          cols="3"
-        >
+        <v-col cols="3">
           <v-img
             height="100%"
           >
@@ -18,10 +16,9 @@
                 cols="12"
               >
                 <v-avatar
-                  class="profile"
+                  class="profile mx-12 my-6"
                   color="grey"
-                  size="164"
-                  tile
+                  size="200"
                 >
                   <v-img
                     src="https://veryimportantlot.com/uploads/over/files/%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8/2020/%D0%9C%D0%B0%D1%80%D1%82%202020/%D0%9C%D0%B0%D1%80%D1%82%20%D0%A1%D1%82%D0%B0%D1%82%D1%8C%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D0%B1%D0%BB%D0%BE%D0%B3%D0%B0.%20%D0%96%D0%B8%D0%B2%D0%BE%D0%BF%D0%B8%D1%81%D1%8C%2063%20(6.1)%20%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82.%20%D0%98%D0%BB%D1%8C%D1%8F%20%D0%A0%D0%B5%D0%BF%D0%B8%D0%BD.%20%C2%AB%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82%20%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B0%20%D0%9C%D0%BE%D0%B4%D0%B5%D1%81%D1%82%D0%B0%20%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D0%B8%D1%87%D0%B0%20%D0%9C%D1%83%D1%81%D0%BE%D1%80%D0%B3%D1%81%D0%BA%D0%BE%D0%B3%D0%BE%C2%BB.jpg"
@@ -31,133 +28,147 @@
             </v-row>
           </v-img>
         </v-col>
-        <v-col cols="4">
-          <div>
-            <h1>
-              New user
-            </h1>
-            <div>
-              <v-btn
-                class="btn_skill"
-                elevation="3"
-                outlined
-                x-small
+        <v-col cols="9">
+          <v-row>
+            <v-col
+              cols="12"
+              class="d-flex"
+            >
+              <div>
+                <h1 class="mb-4">
+                  New user
+                </h1>
+                <div>
+                  <v-btn
+                    class="btn_skill"
+                    elevation="3"
+                    outlined
+                    rounded
+                    small
+                  >
+                    Admin
+                  </v-btn>
+
+                  <v-btn
+                    class="btn_skill"
+                    elevation="3"
+                    outlined
+                    rounded
+                    small
+                  >
+                    Speaker
+                  </v-btn>
+
+                  <v-btn
+                    class="btn_skill"
+                    elevation="3"
+                    outlined
+                    rounded
+                    small
+                  >
+                    AMA
+                  </v-btn>
+                </div>
+                <br>
+              </div>
+              <div
+                class="ml-auto mt-4"
               >
-                Admin
-              </v-btn>
+                <div>
+                  <v-btn
+                    class="btn_social mr-2"
+                    color="secondary"
+                    elevation="3"
+                    icon
+                    x-small
+                  >
+                    <v-icon>mdi-facebook</v-icon>
+                  </v-btn>
 
-              <v-btn
-                class="btn_skill"
-                elevation="3"
-                outlined
-                x-small
+                  <v-btn
+                    color="secondary"
+                    class="btn_social mr-2"
+                    elevation="3"
+                    icon
+                    x-small
+                  >
+                    <v-icon>mdi-twitter</v-icon>
+                  </v-btn>
+
+                  <v-btn
+                    color="secondary"
+                    class="btn_social mr-2"
+                    elevation="3"
+                    icon
+                    x-small
+                  >
+                    <v-icon>mdi-linkedin</v-icon>
+                  </v-btn>
+
+                  <v-btn
+                    color="secondary"
+                    class="btn_social mr-2"
+                    elevation="3"
+                    icon
+                    x-small
+                  >
+                    <v-icon>mdi-google</v-icon>
+                  </v-btn>
+
+                  <v-btn
+                    color="secondary"
+                    class="btn_social"
+                    elevation="3"
+                    icon
+                    x-small
+                  >
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </div>
+                <div>
+                  <v-btn
+                    class="btn_skill"
+                    elevation="2"
+                    text
+                    rounded
+                    small
+                  >
+                    <v-icon class="mr-1">mdi-email</v-icon>
+                    send message
+                  </v-btn>
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="12">
+              <div>
+                <div>
+                  Cheerful resident in this wonderful world.
+                </div>
+                <div>
+                  Minsk, Belarus
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="12">
+              <div
+                class="block ml-auto"
               >
-                Speaker
-              </v-btn>
-
-              <v-btn
-                class="btn_skill"
-                elevation="3"
-                outlined
-                x-small
-              >
-                AMA
-              </v-btn>
-            </div>
-            <br>
-            <div>
-              Cheerful resident in this wonderful world.
-            </div>
-            <div>
-              Minsk, Belarus
-            </div>
-          </div>
-          <div
-            class="block"
-          >
-            <div class="block_follower">
-              <h4>440</h4>
-              <p>followers</p>
-            </div>
-            <div class="block_follower">
-              <h4>31</h4>
-              <p>following</p>
-            </div>
-            <div
-              class="block_follow"
-            >
-              <h2>+ Follow</h2>
-            </div>
-          </div>
-        </v-col>
-        <v-col cols="4">
-          <div class="text-center">
-            <v-btn
-              class="btn_social"
-              color="secondary"
-              elevation="3"
-              icon
-              x-small
-              outlined
-            >
-              1
-            </v-btn>
-
-            <v-btn
-              color="secondary"
-              class="btn_social"
-              elevation="3"
-              icon
-              x-small
-              outlined
-            >
-              1
-            </v-btn>
-
-            <v-btn
-              color="secondary"
-              class="btn_social"
-              elevation="3"
-              icon
-              x-small
-              outlined
-            >
-              1
-            </v-btn>
-
-            <v-btn
-              color="secondary"
-              class="btn_social"
-              elevation="3"
-              icon
-              x-small
-              outlined
-            >
-              1
-            </v-btn>
-
-            <v-btn
-              color="secondary"
-              class="btn_social"
-              elevation="3"
-              icon
-              x-small
-              outlined
-            >
-              1
-            </v-btn>
-          </div>
-          <div>
-            <v-btn
-              elevation="2"
-              text
-              tile
-              outlined
-              x-small
-            >
-              send message
-            </v-btn>
-          </div>
+                <div class="block_follower mr-1">
+                  <h4>440</h4>
+                  <p>followers</p>
+                </div>
+                <div class="block_follower">
+                  <h4>31</h4>
+                  <p>following</p>
+                </div>
+                <div
+                  class="block_follow ml-1"
+                >
+                  <h2>+ Follow</h2>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </div>
@@ -168,7 +179,7 @@
       </v-col>
       <v-col cols="3">
         <button class="rating">
-          inbound marketing
+          <h4>Inbound marketing</h4>
           <v-rating
             readonly
             hover
@@ -181,7 +192,7 @@
       </v-col>
       <v-col cols="3">
         <button class="rating">
-          cool marketing
+          <h4>Cool marketing</h4>
           <v-rating
             readonly
             hover
@@ -194,7 +205,7 @@
       </v-col>
       <v-col cols="3">
         <button class="rating">
-          growth marketing
+          <h4>Growth marketing</h4>
           <v-rating
             readonly
             hover
@@ -209,42 +220,40 @@
     <v-row class="blocks">
       <v-col cols="8">
         <div>
-          <span>Professional Bio</span>
-          <span>10 years of experience</span>
-          <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-          <br>
-          <div>
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book.
-          </div>
-          <br>
-          <div>
-            It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-            desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
-            over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          <div class="d-flex">
+            <div>
+              <h3>Professional Bio</h3>
+            </div>
+            <div class="ml-auto">
+              <h4 class="mr-12">10 years of experience</h4>
+            </div>
           </div>
         </div>
       </v-col>
-      <v-col cols="2">
-        where i write
-        <ul>
-          <li>online.by</li>
-          <li>tut.by</li>
-          <li>relax.by</li>
-          <li>takeout.by</li>
-        </ul>
+      <v-col cols="4">
+        <h3>Where I write</h3>
+      </v-col>
+      <v-col cols="8">
+        <p>Most of all in my life I love programming</p>
+        <p>More than 10,000 books on this topic have been read and courses taken. I am the most pumped-up man</p>
+        <p>If you look at the code that you wrote more than half a year ago, then, most likely, it will seem to you that
+          the author is someone else.
+          Programming is like sex: one mistake and you have to support your whole life.
+        </p>
+      </v-col>
+      <v-col
+        cols="2"
+      >
+        <p>Indound.org</p>
+        <p>On Startups</p>
+        <p>Linked In</p>
+        <p>HubSpot</p>
       </v-col>
       <v-col cols="2">
-        <br>
-        <ul>
-          <li>random text</li>
-          <li>random text</li>
-          <li>random text</li>
-          <li>random text</li>
-        </ul>
+        <p>public author</p>
+        <p>personal blog</p>
+        <p>personal blog</p>
+        <p>company blog</p>
       </v-col>
     </v-row>
   </div>
@@ -258,14 +267,14 @@ export default {
 
 <style lang="scss" scoped>
 .blocks_test {
+  height: 321px;
   width: 100%;
   background-color: #e8f6fa;
-  padding-bottom: 24px;
   padding-top: 28px;
 }
 
 .blocks {
-  width: 85%;
+  width: 90%;
   justify-content: center;
   align-items: center;
   margin-left: auto;
@@ -275,25 +284,29 @@ export default {
 
 .block {
   display: flex;
-  position: absolute;
-  background-color: white;
+  justify-content: flex-end;
 
   .block_follower {
-    width: 165px;
+    width: 200px;
     height: 50px;
     border: 1px solid #e0e0e0;
     text-align: center;
     padding-top: 5px;
+    background-color: #ffffff;
+    border-radius: 50px;
+    cursor: pointer;
   }
 
   .block_follow {
-    width: 330px;
+    width: 370px;
     height: 50px;
     border: 1px solid #e0e0e0;
     text-align: center;
     padding-top: 10px;
     background-color: #3aabf5;
     color: white;
+    border-radius: 50px;
+    cursor: pointer;
   }
 }
 
@@ -316,10 +329,10 @@ export default {
 
 .btn_skill {
   border: 1px orange solid;
+  background-color: white;
 }
 
 .btn_social {
-  background-color: #fdfd86;
   margin-bottom: 35px;
 }
 
